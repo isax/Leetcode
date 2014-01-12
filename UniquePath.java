4ffa7dd936396f7dfb7b7338345e5c2b60b89a02
isax <chengyan.isa@gmail.com>
2014-01-12 12:27:17 -0500

    Update UniquePath.java

--- UniquePath.java
+++ UniquePath.java
@@ -8,8 +8,9 @@ public class UniquePath {
         if(m==1&&n==1) return 1;
         if(m<1||n<1) return 0;
         
-        if(map[m][n]==0) map[m][n] = uniquePaths(m-1, n) + uniquePaths(m, n-1);
+        if(map[m][n]==0) 
+            map[m][n] = uniquePaths(m-1, n) + uniquePaths(m, n-1);
+            
         return map[m][n];
-        }
     }
 }
