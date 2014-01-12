95922a4ff1e4e74324d9ef4f18af2bb31b57a61c
isax <chengyan.isa@gmail.com>
2014-01-12 12:20:14 -0500

    Create UniquePath.java

---
+++ UniquePath.java
@@ -0,0 +1,15 @@
+public class UniquePath {
+
+    public int [][] map = new int[101][101];
+    /*
+	  * DP Top-down tracking
+	  */
+    public int uniquePaths(int m, int n) {
+        if(m==1&&n==1) return 1;
+        if(m<1||n<1) return 0;
+        
+        if(map[m][n]==0) map[m][n] = uniquePaths(m-1, n) + uniquePaths(m, n-1);
+        return map[m][n];
+        }
+    }
+}
