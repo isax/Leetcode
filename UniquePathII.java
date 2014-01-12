6b66919574b424c80c23faef99795d25516a1972
isax <chengyan.isa@gmail.com>
2014-01-12 16:56:25 -0500

    Update UniquePathII.java

--- UniquePathII.java
+++ UniquePathII.java
@@ -25,4 +25,35 @@ public class UniquePathII {
             
         return map[m][n];
     }
+    
+    /*
+     * DP, Bottom-up approach
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0]==1) return 0;
+
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        int [][] map = new int[m][n];
+        map[0][0] = 1;
+        
+        for(int i = 1; i<m; i++){ //start from 1, not 0
+            if(obstacleGrid[i][0]==0) map[i][0] = map[i-1][0]; // easy to make mistake, not just = 1 anymore
+            else map[i][0] = 0;
+        }
+        for(int j = 1; j<n; j++){
+            if(obstacleGrid[0][j]==0) map[0][j] = map[0][j-1];
+            else map[0][j] = 0;
+        }
+        
+        for(int i = 1; i<m;i++){
+            for(int j = 1; j<n;j++){
+                if(obstacleGrid[i][j]==0) map[i][j] = map[i-1][j]+map[i][j-1];
+                else map[i][j] = 0;
+            }
+        }
+        
+        return map[m-1][n-1];
+    }
 }
