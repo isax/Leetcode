fb6b076815d37e149729e6a330beaa483f657e51
isax <chengyan.isa@gmail.com>
2014-01-13 13:28:48 -0500

    Update Subsets.java

--- Subsets.java
+++ Subsets.java
@@ -1,4 +1,5 @@
 public class Subsets {
+	
     public static ArrayList<ArrayList<Integer>> subsets(int[] S) {
 		if(S==null) return null;
 		
@@ -7,6 +8,9 @@ public class Subsets {
 		return res;
     }
 	
+	/*
+	 * Subsets of ArrayList which starts from 0 to index
+	 */
 	public static ArrayList<ArrayList<Integer>> subsetsHelper(int []S, int index){
 				
 		ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
@@ -29,4 +33,43 @@ public class Subsets {
 		
 		return res;
 	}
+	
+	/*
+	 * My intuitive way of thinking: inefficient. Have to copy array every recursion. 
+	 */
+	/*
+	public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        if(S==null) return null;
+        
+        Arrays.sort(S);
+		
+		ArrayList<ArrayList <Integer>> res = new ArrayList<ArrayList<Integer>>();
+		
+		if(S.length==0) {
+			ArrayList<Integer>subset = new ArrayList<Integer>();
+			//subset.add(0);
+			res.add(subset);
+			return res;
+		}
+		
+		int [] subS = new int[S.length-1];		
+		for(int i = 0; i<S.length-1;i++){
+			subS[i] = S[i];
+		}
+		
+		int last = S[S.length-1];
+		
+		ArrayList<ArrayList<Integer>> prev = subsets(subS);
+		
+		for(ArrayList<Integer>list: prev){			
+			res.add(list);
+			ArrayList<Integer>addition = new ArrayList<Integer>();
+			addition.addAll(list);			
+			addition.add(last);
+			res.add(addition);
+		}
+		
+		return res;
+                      
+    }*/
 }
