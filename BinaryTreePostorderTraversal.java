0ca36b5a3a90f23853e4277f307c6c0290bf2325
isax <chengyan.isa@gmail.com>
2014-01-20 00:13:31 -0500

    Create BinaryTreePostorderTraversal.java

---
+++ BinaryTreePostorderTraversal.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if(root==null) return res;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        TreeNode head = root;// cannot be initialized to null, 
+                          //because first iteration need to check if(cur.right==head) e.g.{1, 2}
+        while(!stack.isEmpty()){
+            TreeNode cur = stack.peek();
+            if((cur.left==null&&cur.right==null)|| cur.right == head|| (cur.right==null&&cur.left==head)){
+                head = cur;
+                res.add(stack.pop().val);
+            }else{
+                if(cur.right!=null) stack.push(cur.right);
+                if(cur.left!=null) stack.push(cur.left);
+            }
+        }
+        return res;
+    }
+}
