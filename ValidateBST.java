e34ad0c5bc783c840eaad6e636cd2a7995d0ede7
isax <chengyan.isa@gmail.com>
2014-01-21 12:21:09 -0500

    Create ValidateBST.java

---
+++ ValidateBST.java
@@ -0,0 +1,18 @@
+public class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        ArrayList<Integer>a = new ArrayList<Integer>();
+		inorder(root, a);
+		
+		for(int i = 1; i<a.size(); i++){
+			if(a.get(i-1)>=a.get(i)) return false;
+		}
+		return true;
+    }
+    
+    public static void inorder(TreeNode root, ArrayList<Integer>a){
+		if(root==null) return;
+		inorder(root.left, a);	
+		a.add(root.val);
+		inorder(root.right, a);	
+	}
+}
