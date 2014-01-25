5c991e863c5e26cfc500dc44c4f037cb57e66fc1
isax <chengyan.isa@gmail.com>
2014-01-25 18:15:13 -0500

    Update BST: SymmetricTree.java

--- BST: SymmetricTree.java
+++ BST: SymmetricTree.java
@@ -10,4 +10,40 @@ public class SymmetricTree {
            return false;
         return helper(left.left, right.right)&&helper(left.right, right.left);
     }
+
+    /*
+	 * Just list a wrong in-order method here.
+	 * In-order is not a solution for this problem.
+	 * It will return true for unsymmetric test cases like this: {1,2,3,3,#,2,#}
+	 * The res list is: -2147483648 3 -2147483648 2 -2147483648 1 -2147483648 2 -2147483648 3 -2147483648
+	 * which is symmetric.
+	 */
+	public static boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        
+        ArrayList<Integer>list = new ArrayList<Integer>();
+        inorder(root, list);
+        
+        int i = 0;
+        int j = list.size()-1;
+        
+        while(i!=j){
+            int left = list.get(i);
+            int right = list.get(j);
+            if(left!=right) 
+               return false;
+            i++; j--;
+        }
+        return true;
+    }
+    
+    public static void inorder(TreeNode root, ArrayList<Integer>list){
+        if(root==null){
+            list.add(Integer.MIN_VALUE);
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.value);
+        inorder(root.right, list); 
+    }
 }
