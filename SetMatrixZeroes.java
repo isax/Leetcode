f345601fdcde705c9c0fa3854149c654191e237d
isax <chengyan.isa@gmail.com>
2014-01-31 18:34:22 -0500

    Update SetMatrixZeroes.java

--- SetMatrixZeroes.java
+++ SetMatrixZeroes.java
@@ -22,4 +22,59 @@ public class SetMatrixZeroes {
             }
         }
     }
+    
+    /*
+     * Space: O(1)
+     */
+    public void setZeroes(int[][] matrix) {
+        if(matrix==null) return;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        boolean fstrow = false;
+        boolean fstcol = false;
+        
+        for(int i = 0; i<col; i++){
+            if(matrix[0][i]==0){
+                fstrow = true;
+                break;
+            }
+        }
+        
+        for(int i = 0; i<row; i++){
+            if(matrix[i][0]==0){
+                fstcol = true;
+                break;
+            }
+        }
+        
+        for(int i = 1; i<row; i++){
+            for(int j = 1; j<col; j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        
+        // set zeroes except the first row and column
+        // Notice: cannot start from 0, otherwise ruin the flags
+        for(int i = 1; i<row; i++){
+            for(int j = 1; j<col; j++){
+                if(matrix[0][j]==0||matrix[i][0]==0) matrix[i][j] = 0;
+            }
+        }
+        
+        if(fstrow){
+            for(int i = 0; i<col; i++){
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if(fstcol){
+            for(int i = 0; i<row; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
