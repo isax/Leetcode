aa3e8a88612ff640a741c56e1eb30d9aa01ba58b
isax <isax@users.noreply.github.com>
2014-02-23 17:53:14 -0500

    Create RemoveDuplicatesfromSortedArrayII.java

---
+++ RemoveDuplicatesfromSortedArrayII.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        if(A==null ||A.length==0) return 0;
+        
+        int index = 0;
+        int count = 1;
+        
+        for(int i = 1; i<A.length; i++){
+            if(A[i]!=A[index]){
+                index++;
+                A[index] = A[i];
+                count = 1;
+            }else{
+                if(count==1){
+                    index++;
+                    A[index] = A[i];
+                    count++;
+                }
+            }
+        }
+        return index+1;
+    }
+}
