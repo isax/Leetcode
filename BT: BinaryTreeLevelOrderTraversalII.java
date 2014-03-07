4d2d984dcf33a0a88e4b986e9231229789d782f3
isax <isax@users.noreply.github.com>
2014-03-07 16:19:38 -0500

    Create BT: BinaryTreeLevelOrderTraversalII.java

---
+++ BT: BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,40 @@
+public class BinaryTreeLevelOrderTraversalII {
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        
+        if(root==null) return res;
+        
+        Queue <TreeNode> queue = new LinkedList<TreeNode>();
+        ArrayList<Integer> level = new ArrayList<Integer>();
+        queue.add(root);
+        
+        int curLevel = 1;
+        int nextLevel = 0;
+        
+        while(!queue.isEmpty()){
+            TreeNode cur  = queue.remove();
+            level.add(cur.val);
+            curLevel--;
+            
+            if(cur.left!=null) {
+                queue.add(cur.left);
+                nextLevel++;
+            }
+            if(cur.right!=null) {
+                queue.add(cur.right);
+                nextLevel++;
+            }
+            
+            if(curLevel==0){
+                res.add(level);
+                level = new ArrayList<Integer>();
+                curLevel = nextLevel;
+                nextLevel = 0;
+            }
+            
+        }
+        
+        Collections.reverse(res); // attention cannot return Collections.reverse(res)
+        return res;
+    }
+}
