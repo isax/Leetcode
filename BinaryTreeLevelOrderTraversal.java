3153d9166ae50c4b87c7d6d936087530f1165ded
isax <isax@users.noreply.github.com>
2014-03-07 16:56:02 -0500

    Update BinaryTreeLevelOrderTraversal.java

--- BinaryTreeLevelOrderTraversal.java
+++ BinaryTreeLevelOrderTraversal.java
@@ -33,4 +33,31 @@ public class BinaryTreeLevelOrderTraversal {
         }
         return res;
     }
+    
+    /*
+     * DFS
+     */
+    public ArrayList<ArrayList<Integer>> levelOrderDFS(TreeNode root) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        
+        if(root==null) return res;
+        
+        DFShelper(root, 0, res);
+        return res;
+    }
+    
+    public void DFShelper(TreeNode root, int level, ArrayList<ArrayList<Integer>> res){
+        if(root==null) return;
+        
+        ArrayList<Integer> levelList;
+        if(res.size()==level){
+           levelList = new ArrayList<Integer>();
+           res.add(levelList);
+        }else{
+           levelList = res.get(level); 
+        }
+        levelList.add(root.val);
+        DFShelper(root.left, level+1, res);
+        DFShelper(root.right, level+1, res);
+    }
 }
