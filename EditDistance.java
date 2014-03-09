9104ee410ad21e3faa29034fbd657cd60444f0db
isax <isax@users.noreply.github.com>
2014-03-08 21:25:32 -0500

    Create EditDistance.java

---
+++ EditDistance.java
@@ -0,0 +1,29 @@
+public class EditDistance {
+    public int min3(int a, int b, int c){
+        int m = Math.min(a, b);
+        return Math.min(m, c);
+    }
+    
+    public int minDistance(String word1, String word2) {
+        int dp[][] = new int[word1.length()+1][word2.length()+1];
+        for(int i = 0; i<word1.length()+1; i++){
+            dp[i][0] = i;
+        }
+        
+        for(int j = 0; j<word2.length()+1; j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i = 1; i<word1.length()+1; i++){
+            for(int j = 1; j<word2.length()+1; j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = min3(dp[i][j-1], dp[i-1][j],dp[i-1][j-1])+1; 
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
