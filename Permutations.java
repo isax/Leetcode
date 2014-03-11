7f3e31a31922c2db054e345fb8bcb0daa0c3a380
isax <isax@users.noreply.github.com>
2014-03-11 00:00:50 -0400

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -27,4 +27,41 @@ public class Solution {
         }
         return res;
     }
+    
+    
+    /*
+     * dfs swap
+     */
+     public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        helper(num, 0, res);
+        return res;
+    }
+    
+    public void helper(int[]num, int start, ArrayList<ArrayList<Integer>> res){
+        if(start==num.length){
+            ArrayList<Integer> list = convert(num);
+            res.add(list);
+        }
+        
+        for(int i = start; i<num.length;i++){
+                swap(num, start, i);
+                helper(num, start+1, res);
+                swap(num, start, i);
+        }
+    }
+    
+    public ArrayList<Integer> convert(int[]num){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i:num){
+            list.add(i);
+        }
+        return list;
+    } 
+    
+    public void swap(int []num, int i, int j){
+        int temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
 }
