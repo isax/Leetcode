356943059b2576eedb60b29d49af5ba4741a0bbe
isax <isax@users.noreply.github.com>
2014-03-14 21:04:48 -0400

    Update BT: BinaryTreeInorderTraversal.java

--- BT: BinaryTreeInorderTraversal.java
+++ BT: BinaryTreeInorderTraversal.java
@@ -12,6 +12,28 @@ public class BinaryTreeInorderTraversal {
         helper(root.left, list);
         list.add(root.val);
         helper(root.right, list);
+    }
+    
+    /*
+     * Iterative
+     */
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if(root==null) return res;
         
+        TreeNode cur = root;
+        while(!st.isEmpty() || cur !=null ){
+            while(cur!=null){
+                st.push(cur);
+                cur = cur.left;
+            }
+            
+            cur = st.pop();
+            res.add(cur.val);
+            cur = cur.right;
+            
+        }
+        return res;
     }
 }
